db1603b19ae362432b549dbbf3349c9ccfa48a62
Emilio Cafe' Nunes <shinil1135@gmail.com>
2018-04-18 16:12:35 +0200

    Aggiunta prima versione della navigation bar con switch dei fragment

---
+++ app/src/main/java/com/lucreziagrassi/androidapp/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.lucreziagrassi.androidapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/lucreziagrassi/androidapp/TimerFragment.java
+++ app/src/main/java/com/lucreziagrassi/androidapp/TimerFragment.java
@@ -7,14 +7,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class TestFragment extends Fragment {
+public class TimerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_test, container, false);
+        View view = inflater.inflate(R.layout.fragment_timer, container,false);
         return view;
     }
 }
