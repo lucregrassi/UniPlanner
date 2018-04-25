a070a7c209a4537a5cf890f2faa9a52a162f7676
Emilio Cafe' Nunes <shinil1135@gmail.com>
2018-04-25 21:53:52 +0200

    Fix avvio sulla home

--- app/src/main/java/com/lucreziagrassi/androidapp/MainActivity.java
+++ app/src/main/java/com/lucreziagrassi/androidapp/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        navigationView.setCheckedItem(R.id.nav_home);
+        getFragmentManager().beginTransaction().replace(R.id.content, new HomeFragment()).commit();
     }
 
     @Override
