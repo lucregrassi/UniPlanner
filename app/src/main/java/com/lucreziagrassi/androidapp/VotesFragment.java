ca639f9251e58fd45eea9c6bdfffab4bcae2819b
Lucrezia Grassi <lucre.grassi@gmail.com>
2018-06-07 16:35:33 +0200

    Aggiunti fragment per orario settimanale e per lista voti (ovviamente da completare, fanno cagare)

---
+++ app/src/main/java/com/lucreziagrassi/androidapp/VotesFragment.java
@@ -0,0 +1,72 @@
+package com.lucreziagrassi.androidapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class VotesFragment extends Fragment {
+
+    
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public VotesFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment VotesFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VotesFragment newInstance(String param1, String param2) {
+        VotesFragment fragment = new VotesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_votes, container, false);
+    }
+
+    //NON SO A CHE CAZZO SERVE
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getActivity().setTitle(R.string.votes_fragment_name);
+        if (savedInstanceState != null) {
+            // Restore the fragment's state here
+        }
+    }
+
+}
