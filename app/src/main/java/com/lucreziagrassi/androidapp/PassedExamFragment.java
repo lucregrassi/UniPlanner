8dbe5b4a4c50e5c170423c934035a161872db62b
Lucrezia Grassi <lucre.grassi@gmail.com>
2018-08-01 19:59:19 +0200

    Rename

--- app/src/main/java/com/lucreziagrassi/androidapp/PassedExamFragment.java
+++ app/src/main/java/com/lucreziagrassi/androidapp/PassedExamFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link NewExamFragment.OnFragmentInteractionListener} interface
+ * {@link PassedExamFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link NewExamFragment#newInstance} factory method to
+ * Use the {@link PassedExamFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewExamFragment extends Fragment {
+public class PassedExamFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class NewExamFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public NewExamFragment() {
+    public PassedExamFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class NewExamFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NewExamFragment.
+     * @return A new instance of fragment PassedExamFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NewExamFragment newInstance(String param1, String param2) {
-        NewExamFragment fragment = new NewExamFragment();
+    public static PassedExamFragment newInstance(String param1, String param2) {
+        PassedExamFragment fragment = new PassedExamFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class NewExamFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getActivity().setTitle(R.string.new_exam_fragment_name);
-        View view = inflater.inflate(R.layout.new_exam, container, false);
+        View view = inflater.inflate(R.layout.passed_exam, container, false);
         getActivity().setTitle(R.string.new_exam_fragment_name);
         return view;
     }
