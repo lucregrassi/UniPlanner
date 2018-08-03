0538141d21436b8868cf2efc5e106b93fac2c9c9
Emilio Cafe' Nunes <shinil1135@gmail.com>
2018-08-03 14:52:05 +0200

    Aggiunto versioning schema db

--- app/src/main/java/com/lucreziagrassi/androidapp/db/AppDatabase.java
+++ app/src/main/java/com/lucreziagrassi/androidapp/db/AppDatabase.java
@@ -3,7 +3,7 @@ package com.lucreziagrassi.androidapp.db;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {Exam.class}, version = 1, exportSchema = false)
+@Database(entities = {Exam.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase{
     public abstract ExamDao getExamDao();
 }
