ef24f82a6d21c1822dd7afa4001a4d29bc53c519
Lucrezia Grassi <lucre.grassi@gmail.com>
2018-08-06 21:57:28 +0200

    Modificato layout intro

--- app/src/main/java/com/lucreziagrassi/androidapp/futureExams/FutureExamFragment.java
+++ app/src/main/java/com/lucreziagrassi/androidapp/futureExams/FutureExamFragment.java
@@ -64,8 +64,9 @@ public class FutureExamFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.future_exam, container, false);
+        View view = inflater.inflate(R.layout.future_exam, container, false);
+        getActivity().setTitle(R.string.new_exam_fragment_name);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
