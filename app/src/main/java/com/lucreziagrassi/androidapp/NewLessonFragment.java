c6f2b151487b3beccdd432456280ff62800bc5ba
Lucrezia Grassi <lucre.grassi@gmail.com>
2018-08-08 23:30:13 +0200

    Aggiunto color picker ma non funziona

--- app/src/main/java/com/lucreziagrassi/androidapp/NewLessonFragment.java
+++ app/src/main/java/com/lucreziagrassi/androidapp/NewLessonFragment.java
@@ -1,63 +1,27 @@
 package com.lucreziagrassi.androidapp;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import yuku.ambilwarna.AmbilWarnaDialog;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NewLessonFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NewLessonFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class NewLessonFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class NewLessonFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public NewLessonFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewLessonFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewLessonFragment newInstance(String param1, String param2) {
-        NewLessonFragment fragment = new NewLessonFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    RelativeLayout mLayout;
+    Integer mDefaultColor;
+    Button mButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -66,43 +30,36 @@ public class NewLessonFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.new_lesson, container, false);
         getActivity().setTitle(R.string.new_lesson_fragment_name);
+
+        mLayout = getActivity().findViewById(R.id.new_subject_layout);
+        mDefaultColor = ContextCompat.getColor(getActivity(), R.color.colorPrimary);
+        mButton = getActivity().findViewById(R.id.color_picker_button);
+        mButton.setOnClickListener(this);
+
         return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onClick(View view) {
+        openColorPicker();
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void openColorPicker(){
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(getActivity(), mDefaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mDefaultColor = color;
+                mLayout.setBackgroundColor(color);
+            }
+        });
+        colorPicker.show();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
