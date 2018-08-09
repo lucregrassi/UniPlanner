9f5c13c56f9fa4ef230cec0b0b48e9017a586ffe
Emilio Cafe' Nunes <shinil1135@gmail.com>
2018-08-09 22:41:32 +0200

    Pulite classi fragment, fix color picker

--- app/src/main/java/com/lucreziagrassi/androidapp/NewLessonFragment.java
+++ app/src/main/java/com/lucreziagrassi/androidapp/NewLessonFragment.java
@@ -13,52 +13,23 @@ import android.widget.Toast;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
-public class NewLessonFragment extends Fragment implements View.OnClickListener {
-
-    RelativeLayout mLayout;
-    Integer mDefaultColor;
-    Button mButton;
-
+public class NewLessonFragment extends Fragment
+{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.new_lesson, container, false);
         getActivity().setTitle(R.string.new_lesson_fragment_name);
 
-        mLayout = getActivity().findViewById(R.id.new_subject_layout);
-        mDefaultColor = ContextCompat.getColor(getActivity(), R.color.colorPrimary);
-        mButton = getActivity().findViewById(R.id.color_picker_button);
-        mButton.setOnClickListener(this);
-
         return view;
     }
 
-    @Override
-    public void onClick(View view) {
-        openColorPicker();
-    }
-
-    public void openColorPicker(){
-        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(getActivity(), mDefaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
-            @Override
-            public void onCancel(AmbilWarnaDialog dialog) {
-
-            }
-
-            @Override
-            public void onOk(AmbilWarnaDialog dialog, int color) {
-                mDefaultColor = color;
-                mLayout.setBackgroundColor(color);
-            }
-        });
-        colorPicker.show();
-    }
 
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
